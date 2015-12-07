f58d205752c3c81f605ca8bdcad594f347c73b9a
hongkyun.kim <hongkyun.kim@lge.com>
2015-12-07 10:05:55 +0900

    hello.c modified on Local repository

--- hello.c
+++ hello.c
@@ -7,4 +7,7 @@ printf("helloworld\n");
 printf("Tell your world\n");
 printf("Tell his world"\n);
 printf("Tell her world \n");
+printf("Git Tutorial\n");
+//For Command line git tutorial
+//Local repository modification
 }
