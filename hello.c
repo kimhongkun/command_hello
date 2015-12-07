c9d5eb3b2b4a0c9451196ace20328d28cf090d83
kimhongkun <kimhongkun@naver.com>
2015-12-07 10:03:35 +0900

    Update hello.c

--- hello.c
+++ hello.c
@@ -7,4 +7,8 @@ printf("helloworld\n");
 printf("Tell your world\n");
 printf("Tell his world"\n);
 printf("Tell her world \n");
+printf("Git Tutorial\n");
+//For command line git tutorial
+//GitHub modification
+
 }
