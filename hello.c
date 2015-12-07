585338576409f39e4a23f68f8aba733af590e1bd
kimhongkun <kimhongkun@naver.com>
2015-12-07 10:14:47 +0900

    GitHub for git pull

--- hello.c
+++ hello.c
@@ -7,6 +7,7 @@ printf("helloworld\n");
 printf("Tell your world\n");
 printf("Tell his world"\n);
 printf("Tell her world \n");
+printf("Git Pull Test\n");
 //For Command line git tutorial
 //Local repository modification
 //GitHub modification
